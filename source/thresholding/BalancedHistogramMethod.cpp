0ce747dbc3902035da3de0e5ebd2f8f7da66b321
Tom Svoboda <tsvob94@gmail.com>
2017-04-13 08:53:22 +0200

    3 basic algorithm written (not tested)

--- source/thresholding/BalancedHistogramMethod.cpp
+++ source/thresholding/BalancedHistogramMethod.cpp
@@ -13,10 +13,37 @@ namespace thresholding {
         name = "Balanced Histogram Thresholding Method";
     }
 
+    int BalancedHistogramMethod::getBHThreshold() {
+        int lIndex = 0;   // min index
+        int rIndex = 255; // max index
+
+        int lWeight = histogram.at(lIndex);
+        int rWeight = histogram.at(rIndex);
+
+        while(lIndex < rIndex) {
+            if(rWeight > lIndex) {
+                // Right side is heavier -> push right index to left
+                rIndex--;
+                rWeight += histogram.at(rIndex);
+            }
+            else {
+                // Left side is heavier -> push left index into right
+                lIndex++;
+                lWeight += histogram.at(lIndex);
+            }
+        }
+
+        return lIndex;
+    }
+
     void BalancedHistogramMethod::run(cv::Mat& result) {
         result = cv::Mat( image.size(), image.type() );
 
-        // @TODO
+        // Get threshold
+        unsigned char threshold = getBHThreshold();
+
+        // Thresholding
+        performThresholding(threshold, result);
     }
 
 }
